ec4bb051215ff04987c91796476605e981e7e3f6
tstih <tomaz.stih@wischner.co.uk>
2025-04-01 21:38:04 +0100

    Added signal.

--- include/native.h
+++ include/native.h
@@ -1,11 +1,60 @@
 #pragma once
+
 #include <string>
 #include <vector>
+#include <functional>
+#include <map>
+#include <utility>
 
 extern int program(int argc, char **argv);
 
 namespace native
 {
+    template <typename... Args>
+    class signal
+    {
+    public:
+        signal();
+        explicit signal(std::function<void()> init);
+
+        int connect(const std::function<bool(Args...)> &slot) const;
+
+        template <typename T>
+        int connect(T *instance, bool (T::*method)(Args...));
+
+        template <typename T>
+        int connect(const T *instance, bool (T::*method)(Args...) const);
+
+        void disconnect(int id) const;
+        void disconnect_all() const;
+        void emit(Args... args);
+
+    private:
+        void ensure_init() const;
+
+        mutable std::map<int, std::function<bool(Args...)>> slots;
+        mutable int current_id;
+        mutable bool initialized;
+        std::function<void()> initializer;
+    };
+
+    // Inline templated member functions
+    template <typename... Args>
+    template <typename T>
+    int signal<Args...>::connect(T *instance, bool (T::*method)(Args...))
+    {
+        return connect([=](Args... args)
+                       { return (instance->*method)(std::forward<Args>(args)...); });
+    }
+
+    template <typename... Args>
+    template <typename T>
+    int signal<Args...>::connect(const T *instance, bool (T::*method)(Args...) const)
+    {
+        return connect([=](Args... args)
+                       { return (instance->*method)(std::forward<Args>(args)...); });
+    }
+
     class app_wnd;
 
     class app final
